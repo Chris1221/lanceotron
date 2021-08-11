bbcff757c13ccf2b452cb55633cf219f2309ec1c
Christopher Cole <ccole@well.ox.ac.uk>
2021-08-11 06:58:55 -0400

    Test resource availability and model construction

--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,17 @@
 import unittest
+import lanceotron.utils 
+import pkg_resources
+import os
+
+import numpy as np
+
 from lanceotron import find_and_score_peaks, dotdict
+from tensorflow import keras
 
 # Simple run through on toy data
 class TestAll(unittest.TestCase):
+    """Full run through on toy data
+    """
     def test_example(self):
         find_and_score_peaks(dotdict({
             "file": "test/chr22.bw",
@@ -11,3 +20,26 @@ class TestAll(unittest.TestCase):
             "window": 400,
             "skipheader": False
         }))
+
+class TestResources(unittest.TestCase):
+    """Ensure that all of the resources are available.
+    """
+    def test_model(self):
+        assert os.path.isfile(pkg_resources.resource_filename("lanceotron.static", "wide_and_deep_fully_trained_v5_03.h5"))
+    def test_widescaler(self):
+        assert os.path.isfile(pkg_resources.resource_filename('lanceotron.static', 'standard_scaler_wide_v5_03.p'))
+    def test_deepscaler(self):
+        assert os.path.isfile(pkg_resources.resource_filename('lanceotron.static', 'standard_scaler_deep_v5_03.p'))
+
+
+class TestModel(unittest.TestCase):
+    """Ensure that the model is correctly formed
+    """
+    def setUp(self):
+        self.model = lanceotron.utils.build_model()
+    def test_nlayer(self):
+        assert len(self.model.layers) == 37, "Did the model change? It has the wrong number of layers."
+    def test_lastlayer(self):
+        assert isinstance(self.model.layers[-1], keras.layers.Dense), "The last layer is not a dense prediction layer. Did the model load correctly?"
+    def test_hasweights(self):
+        np.testing.assert_almost_equal(self.model.layers[-1].get_weights()[1][1], -0.015733806, err_msg= "The model does not have the correct weights. Check the weight file.")
